9232c60b9fdf7471495c5e776fdddb8788c9d678
Julia Prestes <71856252+juliaprestes@users.noreply.github.com>
2021-10-15 13:06:51 -0300

    Add files via upload

---
+++ Fatorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto Fatorial!");
+            int fatorial = 1;
+            for (int n = 1; n <= 10; n++)
+            {
+                fatorial = n * fatorial;
+                Console.WriteLine(n + " fatorial = " + fatorial);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ multiplosDeTres/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace multiplosDeTres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Executando projeto multiplos de 3");
+            for(numero =1; numero <=100; numero++)
+            {
+                if (numero % 3 == 0)
+                    Console.Write(numero+" ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
