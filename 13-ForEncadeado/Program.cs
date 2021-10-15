4c418f1aa1ec720fbf1f5144a715afe537d57549
Julia Prestes <71856252+juliaprestes@users.noreply.github.com>
2021-10-15 13:06:01 -0300

    Add files via upload

---
+++ 13-ForEncadeado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 13");
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //******
+
+            //desenho * com break
+            for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+            // forma diferente de fazer
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <=  contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExercicioAliquota/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExercicioAliquota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 3900.0;
+            if (salario >= 1900.0 && salario <= 2800.0)
+            {
+                Console.WriteLine("Seu IR é de 7.5% e pode deduzir na declaração o valor de R$ 142,00 ");
+            }
+            else if (salario >= 2800.01 && salario <= 3751.0)
+            {
+                Console.WriteLine("Seu IR é de 15% e pode deduzir R$ 350,00");
+            }
+            else if(salario >= 3751.01 && salario <= 4664.0)         
+            {
+                Console.WriteLine("Seu IR é de 22.5%, pode deduzir R$ 636,00");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
