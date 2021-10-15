86f2be6842d1f6a98affe22f1bc0a798bff2347a
Julia Prestes <71856252+juliaprestes@users.noreply.github.com>
2021-10-15 13:04:41 -0300

    Add files via upload

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula Poupança!");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while(contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após "+contadorMes+" mês, você terá R$ " + valorInvestido);
+                contadorMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11-CalculoPoupanca2(For)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculoPoupanca2_For_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 11 - laço for!");
+
+            double valorInvestido = 1000;
+
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido = valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$ " + valorInvestido);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
